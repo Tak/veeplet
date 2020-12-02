a71a22894941077fe6d50959696c538043f66038
Levi Bard <taktaktaktaktaktaktaktaktaktak@gmail.com>
2020-12-02 14:03:06 +0100

    Handle the case when resume requests a 2fa code

--- lib/veeplet/ui.rb
+++ lib/veeplet/ui.rb
@@ -2,10 +2,15 @@ require 'gtk3'
 
 module Veeplet
   class UI
-    def self.authenticate()
+    def self.load_builder()
       builder = Gtk::Builder.new()
       builder.add_from_file('ui.glade')
       builder.connect_signals{ |handler| method(handler) }
+      return builder
+    end
+
+    def self.authenticate()
+      builder = load_builder()
 
       dialog = builder['dialog_authentication']
       response = dialog.run()
@@ -19,6 +24,22 @@ module Veeplet
       end
       dialog.destroy()
       yield username, password, two_factor
+    end
+
+    def self.prompt_two_factor()
+      builder = load_builder()
+      ['label_username', 'entry_username', 'label_password', 'label_password'].each do |thing|
+        builder[thing].hide()
+      end
+
+      dialog = builder['dialog_authentication']
+      response = dialog.run()
+      two_factor = nil
+      if response == Gtk::ResponseType::OK
+        two_factor = builder['entry_2fa'].text
+      end
+      dialog.destroy()
+      yield two_factor
     end
   end
 end
